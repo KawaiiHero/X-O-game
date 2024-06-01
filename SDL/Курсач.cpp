47be4337673e4a9dea796cc75e2894ca6810e5ca
Денис Гребенник <grebennik_den@mail.ru>
2024-06-01 22:38:33 +0300

    Добавьте файлы проекта.

---
+++ SDL/Курсач.cpp
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <cmath>
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+
+int main(int argc, char* args[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	TTF_Init();
+	return 1;
+}
